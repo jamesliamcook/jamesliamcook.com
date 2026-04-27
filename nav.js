2e364593e541f1662bcfaa8dfd3f0ca77dcc9e74
jamesliamcook <65237380+jamesliamcook@users.noreply.github.com>
2026-04-27 15:07:31 +0100

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,14 @@
+// Mobile nav toggle
+const toggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.nav-links');
+
+if (toggle && links) {
+  toggle.addEventListener('click', () => {
+    links.classList.toggle('open');
+  });
+
+  // Close on link click
+  links.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', () => links.classList.remove('open'));
+  });
+}
